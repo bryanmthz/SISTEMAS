24985b4b45d363fe7890e9fea1c131773f8eec54
Bryan <bryantrader.17@gmail.com>
2026-01-26 21:22:11 -0300

    primeiro commit

---
+++ projetofairlineovereunder original2.0 copy 19 new model of 12/main.py
@@ -0,0 +1,10 @@
+# projeto/main.py
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui.gui import CalculadoraEstatistica
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = CalculadoraEstatistica()
+    window.show()
+    sys.exit(app.exec_())
